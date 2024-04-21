aa0c24b636042ad9bdaab1a540a3b91b9a9f11f5
Santiago <santyjavivas@hotmail.com>
2024-04-21 11:34:41 -0500

    adding database functionality

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from wfdb_processing.paths import get_bases_and_path_dict
 
-current_penis = "hola"
+WFDB_BASES_NAMES, WFDB_TO_RECORD = get_bases_and_path_dict()
+CURRENT_BASE = WFDB_BASES_NAMES[1]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print(current_penis)
     return render_template("index.html")
 
 @app.route("/plotting")
 def plotting_page():
-    global current_penis
-    current_penis = "chao"
-    print(current_penis)
     return render_template("plotting.html")
 
+@app.route("/wfdb_databases")
+def databases_page():
+    global CURRENT_BASE
+    return render_template("wfdb_databases.html", wfdb_sources = WFDB_BASES_NAMES)
+
+@app.route("/process_data", methods=["POST"])
+def process_data():
+    if request.method == "POST":
+        source = request.form.get("source")  # Get the value of 'source' from the POST request
+        # Process the data as needed
+        # Return a response if needed
+        return "/plotting"
 
 if __name__ == "__main__":
     app.run()
--- static/js/redirecting.js
+++ static/js/redirecting.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    // Target all elements with the class 'nav-link'
     $('.nav-link').click(function(event) {
-        event.preventDefault(); // Prevent default behavior of anchor tag
-        // Get the URL from the href attribute of the clicked element
+        event.preventDefault();
         var redirectUrl = $(this).attr('href');
         if (redirectUrl === "/reporte"){
             if (window.location.pathname === '/plotting'){
@@ -18,9 +16,24 @@ $(document).ready(function() {
     });
 
     $('principal-page-button').click(function(event) {
-        event.preventDefault(); // Prevent default behavior of anchor tag
-        // Get the URL from the href attribute of the clicked element
+        event.preventDefault();
         var redirectUrl = $(this).attr('href');
         window.location.href = redirectUrl;        
+    });
+
+    $('.card-link').click(function(event) {
+        event.preventDefault();
+        var source = $(this).data('source');
+        $.ajax({
+            url: '/process_data',
+            method: 'POST',
+            data: {source: source},
+            success: function(response) {
+                window.location.href = response;
+            },
+            error: function(xhr, status, error) {
+                console.error("Error making POST request:", error);
+            }
+        });
     });
 });
